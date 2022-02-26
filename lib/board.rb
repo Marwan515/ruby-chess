ecbe2887bba8cbbc742fd3b026a61b29d290cfeb
Marwan <Marwanabdulmannan@gmail.com>
2022-02-27 01:27:00 +0400

    added functionality's and deleted older files and one single class to add all functionality's

--- lib/board.rb
+++ lib/board.rb
@@ -1,21 +1,27 @@
 require 'colorize'
-require_relative 'chess_pcs.rb'
 
-module ChessBoard
-    include ChessMoves
-    include Pieces
+class ChessBoard
 
     def initialize
         @grid = Array.new(8) {Array.new(8) {" "}}
-        @white_pcs = pc_arr("white")
-        @black_pcs = pc_arr("black")
+        @white_pcs = [white_rook, white_knight, white_bishop, white_queen, white_king, white_bishop, white_knight, white_rook]
+        @black_pcs = [black_rook, black_knight, black_bishop, black_queen, black_king, black_bishop, black_knight, black_rook]
+        @pind = nil
         @wp = white_pawn
         @bp = black_pawn
-        @pind = nil
         @abcs = {"a" => 0, "b" => 1, "c" => 2, "d" => 3, "e" => 4, "f" => 5, "g" => 6, "h" => 7}
+        @splayer = nil
+        @knight_m = [[2, 1], [2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2], [-2, 1], [-2, -1]]
+        @king_moved = [false, false]
+        @pone_capture = []
+        @ptwo_capture = []
+        @step = true
+        @white_kings_threat = [black_queen, black_bishop, black_rook, black_knight, black_pawn]
+        @black_kings_threat = [white_queen, white_bishop, white_rook, white_knight, white_pawn]
+        make_brd
     end
     # make the board
-    def display_board
+    def make_brd
         @grid.each_with_index do |row, idx|
             row.each_with_index do |a, v|
                 if idx < 1
@@ -28,55 +34,567 @@ module ChessBoard
                     row[v] = @black_pcs[v]
                 end
             end
+        end
+    end
+    def display_board
+        @grid.each_with_index do |row, idx|
             j = idx + 1
             j.to_s
-            puts "#{j} " + row.join(" ").colorize(:background => :black)
+            puts "#{j} " + row.join("|").colorize(:background => :black)
         end
         puts ("  A B C D E F G H")
+        if @pone_capture.length > 0
+            puts "Player One's Captured Pieces: #{@pone_capture}".colorize(:color => :white, :background => :red)
+        elsif @ptwo_capture.length > 0
+            puts "Player two's Captured Pieces: #{@ptwo_capture}".colorize(:color => :white, :background => :blue)
+        end
+        if @splayer == "white"
+            puts "Player ONE".colorize(:color => :white, :background => :red)
+        elsif @splayer == "black"
+            puts "Player TWO".colorize(:color => :white, :background => :blue)
+        end
     end
     # chess piece selection
-    def select_piece
-        sl = {"a" => 0, "b" => 1, "c" => 2, "d" => 3, "e" => 4, "f" => 5, "g" => 6, "h" => 7}
+    def select_piece(player = @splayer)
         puts "Select Your Piece Enter Alphabet And Number, Example: D8 = White King 1"
         puts "Enter Your Selection: "
         x = gets.chomp.downcase
         x = x.split("")
-        col = x[1].to_i - 1
-        row = sl[x[0]]
-        o = [col, row]
-        @pind = o
-        o
-    end
-    # check i-f the position is moveable
-    def opponent(player, idx)
-        a = @white_pcs
-        b = @black_pcs
-        if a.include?(idx) && player == "black"
-            true
-        elsif b.include?(idx) && player == "white"
-            true
-        elsif a.include?(idx) && player == "white"
-            false
-        elsif b.include?(idx) && player == "black"
-            false
+        ro = x[1].to_i
+        rows = ro - 1
+        col = @abcs[x[0]]
+        play_list = nil
+        if player == "white"
+            play_list = @white_pcs
+            play_list << white_pawn
+        elsif player == "black"
+            play_list = @black_pcs
+            play_list << black_pawn
+        end
+        if play_list.include?(@grid[rows][col])
+            m = @grid[rows][col]
+            if m == black_bishop || m == white_bishop
+                h = bishop_moves(rows, col)
+                if h.length < 1
+                    puts "No Possible Moves For This Piece!"
+                    select_piece
+                end
+            elsif m == black_rook || m == white_rook
+                h = rook_moves(rows, col)
+                if h.length < 1
+                    puts "No Possible Moves For This Piece!"
+                    select_piece
+                end
+            elsif m == black_queen || m == white_queen
+                h = queen_moves(rows, col)
+                if h.length < 1
+                    puts "No Possible Moves For This Piece!"
+                    select_piece
+                end
+            elsif m == black_knight || m == white_knight
+                h = knight_moves(rows, col)
+                if h.length < 1
+                    puts "No Possible Moves For This Piece!"
+                    select_piece
+                end
+            elsif m == black_king || m == white_king
+                h = king_moves
+                if h.length < 1
+                    puts "No Possible Moves For This Piece!"
+                    select_piece
+                end
+            elsif m == black_pawn || m == white_pawn
+                h = pawn_moves(rows, col)
+                if h.length < 1
+                    puts "No Possible Moves For This Piece!"
+                    select_piece
+                end
+            end
+            @pind = [rows, col]
         else
+            puts "Its Not Your PIECE!!!!!!"
+            select_piece
+        end
+    end
+
+    def op_player(player = @splayer)
+        play_list = nil
+        if player == "white"
+            play_list = @black_pcs
+            play_list << @bp
+        elsif player == "black"
+            play_list = @white_pcs
+            play_list << @wp
+        end
+        play_list
+    end
+
+    # check tile if its empty
+    def empty_tile(rows, col)
+        op = op_player
+        if @step == false
+            return false
+        elsif @grid[rows].nil?
+            return false
+        elsif @grid[rows][col] == " "
+            return true
+        elsif op.include?(@grid[rows][col])
+            @step = false
+            return true
+        end
+    end
+
+    def player_on(player)
+        @splayer = player
+    end
+
+    def white_king
+        "\u2654"
+    end
+
+    def white_queen
+        "\u2655"
+    end
+
+    def white_knight
+        "\u2658"
+    end
+
+    def white_rook
+        "\u2656"
+    end
+
+    def white_bishop
+        "\u2657"
+    end
+
+    def white_pawn
+        "\u2659"
+    end
+
+    def black_king
+        "\u265a"
+    end
+
+    def black_queen
+        "\u265b"
+    end
+
+    def black_rook
+        "\u265c"
+    end
+
+    def black_bishop
+        "\u265d"
+    end
+
+    def black_knight
+        "\u265e"
+    end
+
+    def black_pawn
+        "\u265f"
+    end
+
+    def pawn_moves(rows, col, player = @splayer)
+        start_pos = nil
+        if rows == 1 || rows == 6
+            start_pos = true
+        else
+            start_pos = false
+        end
+        moves = []
+        if start_pos == true
+            if player == "white"
+                if empty_tile(rows + 2, col)
+                    moves << [rows + 2, col]
+                    if empty_tile(rows + 1, col)
+                        moves << [rows + 1, col]
+                    end
+                end
+            elsif player == "black"
+                if empty_tile(rows - 2, col)
+                    moves << [rows - 2, col]
+                    if empty_tile(rows - 1, col)
+                        moves << [rows -1, col]
+                    end
+                end
+            end
+        else
+            if player == "white"
+                a = [[rows + 1, col + 1], [rows + 1, col - 1]]
+                a.each do |i|
+                    if empty_tile(i[0], i[1])
+                        moves << i
+                    end
+                end
+            elsif player == "black"
+                a = [[rows - 1, col - 1], [rows - 1, col + 1]]
+                a.each do |i|
+                    if empty_tile(i[0], i[1])
+                        moves << i
+                    end
+                end
+            end
+        end
+        moves
+    end
+
+    def rook_moves(rows, col, player = @splayer)
+        moves = []
+        t = rows
+        r = col
+        b = rows
+        l = col
+        while empty_tile(t + 1, col)
+            t += 1
+            moves << [t, col]
+        end
+        while empty_tile(rows, r + 1)
+            r += 1
+            moves << [rows, r]
+        end
+        while empty_tile(b - 1, col)
+            b -= 1
+            moves << [b, col]
+        end
+        while empty_tile(rows, l - 1)
+            l -= 1
+            moves << [rows, l]
+        end
+        if player == "black"
+            moves.reverse
+            moves
+        else
+            moves
+        end
+    end
+
+    def bishop_moves(rows, col, player = @splayer)
+        moves = []
+        tb = rows
+        rl = col
+        while empty_tile(tb + 1, rl + 1)
+            tb += 1
+            rl += 1
+            moves << [tb, rl]
+        end
+        tb = rows
+        rl = col
+        while empty_tile(tb - 1, rl + 1)
+            tb -= 1
+            rl += 1
+            moves << [tb, rl]
+        end
+        tb = rows
+        rl = col
+        while empty_tile(tb - 1, rl - 1)
+            tb -= 1
+            rl -= 1
+            moves << [tb, rl]
+        end
+        tb = rows
+        rl = col
+        while empty_tile(tb + 1, rl - 1)
+            tb += 1
+            rl -= 1
+            moves << [tb, rl]
+        end
+        if player == "black"
+            moves.reverse
+            moves
+        else
+            moves
+        end
+    end
+
+    def knight_moves(rows, col, player = @splayer)
+        moves = []
+        @knight_m.each do |i|
+            if empty_tile(rows + i[0], col + i[1])
+                moves << [rows + i[0], col + i[1]]
+            end
+        end
+        if player == "black"
+            moves.reverse
+        else
+            moves
+        end
+    end
+
+    def queen_moves(rows, col, player = @splayer)
+        moves = []
+        diag = bishop_moves(rows, col)
+        line = rook_moves(rows, col)
+        diag.each { |i| moves << i }
+        line.each { |i| moves << i }
+        moves
+    end
+
+    def king_moves(player = @splayer)
+        moves = []
+        rows = nil
+        k = nil
+        if player == "white"
+            k = white_king
+        else
+            k = black_king
+        end
+        @grid.each_with_index { |i, x| i.include?(k) ? rows = x : next }
+        col = @grid[rows].index(k)
+        if empty_tile(rows + 1, col)
+            moves << [rows + 1, col]
+        elsif empty_tile(rows + 1, col + 1)
+            moves [rows + 1, col + 1]
+        elsif empty_tile(rows, col + 1)
+            moves << [rows, col + 1]
+        elsif empty_tile(rows - 1, col + 1)
+            moves << [rows - 1, col + 1]
+        elsif empty_tile(rows - 1, col)
+            moves << [rows - 1, col]
+        elsif empty_tile(rows - 1, col - 1)
+            moves << [rows - 1, col - 1]
+        elsif empty_tile(rows, col - 1)
+            moves << [rows, col - 1]
+        elsif empty_tile(rows + 1, col - 1)
+            moves << [rows + 1, col - 1]
+        end
+        if player == "black"
+            moves.reverse
+            moves
+        else
+            moves
+        end
+    end
+
+    def checked(player = @splayer)
+        threats = []
+        g = @grid
+        thr = nil
+        kings_row = nil
+        k = nil
+        if player == "white"
+            k = white_king
+            thr = @white_kings_threat
+        else
+            k = black_king
+            thr = @black_kings_threat
+        end
+        @grid.each_with_index { |i, x| i.include?(k) ? kings_row = x : next }
+        kings_col = @grid[kings_row].index(k)
+        diagonal_check = queen_moves(kings_row, kings_col)
+        diagonal_check.each do |x|
+            if thr.include?(g[x[0]][x[1]])
+                threats << [x[0], x[1]]
+            end
+        end
+        threats
+    end
+
+    def check_mate(player = @splayer)
+        list_to_check = checked
+        return false if list_to_check.length < 1
+        g = @grid
+        king_row = nil
+        k = nil
+        if player == "white"
+            k = white_king
+        else
+            k = black_king
+        end
+        @grid.each_with_index { |i, x| i.include?(k) ? king_row = x : next }
+        king_col = @grid[king_row].index(k)
+        k_moves = king_moves
+        if k_moves.length < 1
+            return true
+        else
+            k_moves.each do |k|
+                if !check_row_col(k[0], k[1])
+                    false
+                end
+            end
             true
         end
     end
-    # move the piece
-    def change_place(col, row, pc)
-        @grid[col][row] = pc
-        @grid[pind[0]][pind[1]] = " "
-    end
 
-    def empty_tile(cols, rows)
-        if !@grid[cols][rows] == " "
-            true
+    def check_row_col(rows, col, player = @splayer)
+        g = @grid
+        thr = nil
+        if player == "white"
+            thr = @white_kings_threat
+        else
+            thr = @black_kings_threat
+        end
+        line_check = queen_moves(rows, col)
+        line_check.each do |n|
+            if thr.include?(g[n[0]][n[1]])
+                true
+            end
         end
         false
     end
 
+    def pawn_promotion(player = @splayer)
+        g = @grid
+        promo = false
+        col = nil
+        rows = nil
+        if g[0].include?(black_pawn)
+            rows = 0
+            promo = true
+            col = g[rows].index(black_pawn)
+        elsif g[-1].include?(white_pawn)
+            promo = true
+            rows = 7
+            col = g[rows].index(white_pawn)
+        end
+        j = nil
+        if promo == true
+            puts "Select Your Promotion Piece\nEnter [1] To Promote To Queen\n Enter [2] To Promote To Bishop\nEnter [3] To Promote To Rook\n Enter [4] To Promote To Knight"
+            j = gets.chomp
+            if j == "1"
+                if player == "white"
+                    g[rows][col] = white_queen
+                elsif player == "black"
+                    g[rows][col] = black_queen
+                end
+            elsif j == "2"
+                if player == "white"
+                    g[rows][col] = white_bishop
+                elsif player == "black"
+                    g[rows][col] = black_bishop
+                end
+            elsif j == "3"
+                if player == "white"
+                    g[rows][col] = white_rook
+                elsif player == "black"
+                    g[rows][col] = black_rook
+                end
+            elsif j == "4"
+                if player == "white"
+                    g[rows][col] = white_knight
+                elsif player == "black"
+                    g[rows][col] = black_knight
+                end
+            else
+                puts "Eh Eh Wrong Entry"
+                pawn_promotion
+            end
+        end
+    end
+
+    def captured(piece, player = @splayer)
+        if player == "white"
+            if piece != " "
+                @pone_capture << piece
+            end
+        elsif player == "black"
+            if piece != " "
+                @ptwo_capture << piece
+            end
+        end
+    end
+
+    def moved_piece(rowf, colf, rowt, colt)
+        @step = true
+        captured(@grid[rowt][colt])
+        @grid[rowt].delete_at(colt)
+        @grid[rowt].insert(colt, @grid[rowf][colf])
+        @grid[rowf][colf] = " "
+    end
+
+    def move(player = @splayer)
+        g = @grid
+        current_col = @pind[1]
+        current_row = @pind[0]
+        puts "Enter The Tile To move"
+        j = gets.chomp.split("")
+        col = @abcs[j[0]]
+        rows = j[1].to_i
+        rows -= 1
+        yes = false
+        moveable = nil
+        if g[current_row][current_col] == white_pawn || g[current_row][current_col] == black_pawn
+            moveable = pawn_moves(current_row, current_col)
+            puts moveable
+            moveable.each do |i|
+                if i[0] == rows && i[1] == col
+                    yes = true
+                end
+            end
+            if yes == false
+                puts "Not Moveable Tile Error"
+                move
+            else
+                moved_piece(current_row, current_col, rows, col)
+                pawn_promotion
+            end
+        elsif g[current_row][current_col] == white_knight || g[current_row][current_col] == black_knight
+            moveable = knight_moves(current_row, current_col)
+            moveable.each do |i|
+                if i[0] == rows && i[1] == col
+                    yes = true
+                end
+            end
+            if yes == false
+                puts "Not Moveable Tile Error"
+                move
+            else
+                moved_piece(current_row, current_col, rows, col)
+            end
+        elsif g[current_row][current_col] == white_queen || g[current_row][current_col] == black_queen
+            moveable = queen_moves(current_row, current_col)
+            moveable.each do |i|
+                if i[0] == rows && i[1] == col
+                    yes = true
+                end
+            end
+            if yes == false
+                puts "Not Moveable Tile Error"
+                move
+            else
+                moved_piece(current_row, current_col, rows, col)
+            end
+        elsif g[current_row][current_col] == white_bishop || g[current_row][current_col] == black_bishop
+            moveable = bishop_moves(current_row, current_col)
+            moveable.each do |i|
+                if i[0] == rows && i[1] == col
+                    yes = true
+                end
+            end
+            if yes == false
+                puts "Not Moveable Tile Error"
+                move
+            else
+                moved_piece(current_row, current_col, rows, col)
+            end
+        elsif g[current_row][current_col] == white_rook || g[current_row][current_col] == black_rook
+            moveable = rook_moves(current_row, current_col)
+            moveable.each do |i|
+                if i[0] == rows && i[1] == col
+                    yes = true
+                end
+            end
+            if yes == false
+                puts "Not Moveable Tile Error"
+                move
+            else
+                moved_piece(current_row, current_col, rows, col)
+            end
+        elsif g[current_row][current_col] == white_king || g[current_row][current_col] == black_king
+            moveable = king_moves
+            moveable.each do |i|
+                if i[0] == rows && i[1] == col
+                    yes = true
+                end
+            end
+            if yes == false
+                puts "Not Moveable Tile Error"
+                move
+            else
+                moved_piece(current_row, current_col, rows, col)
+            end
+        end
+    end
+
 end
-x = ChessBoard.new
-x.display_board
-puts x.black_pawn
--- lib/main.rb
+++ lib/main.rb
@@ -1,20 +1,40 @@
+require 'colorize'
 require_relative 'board.rb'
-
 class Chess_Game
 
     def initialize
         @playerone = "white"
         @playertwo = "black"
         @current_player = nil
-        @selected_piece = nil
+        @game = ChessBoard.new
+        @game_end = false
+        intro
+        while @game_end == false
+            player_turns
+            @game.player_on(@current_player)
+            @game.display_board
+            @game.select_piece
+            @game.move
+            
+        end
+        puts "Thanks For Playing!"
     end
 
     def player_turns(player = @current_player)
-        if player == nil
+        if player == nil || player == @playertwo
             @current_player = @playerone
         else
-            @current_player @playertwo
+            @current_player = @playertwo
         end
     end
 
+    def intro
+        puts "This Project Is From TheOdinProject Curriculum"
+        puts "i'm Not Really Good At Chess So If I Missed Something Let me Know\n I Didn't Add The En-pessant Move yet So Sorry To Disapoint You\n Chess GEEKS! \nSo Lets Begin"
+        puts "Thanks For Playing!"
+        puts "My Github: https://github.com/Marwan515"
+    end
 end
+
+start_game = Chess_Game.new
+start_game
